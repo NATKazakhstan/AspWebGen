4f933881bceb74bd28563ea9270cfe95ee376fab
Sergey.Shpakovskiy <Sergey.Shpakovskiy@nat.kz>
2017-10-11 13:47:02 +0600

    Nat.Web.Controls - исправлена проблема с конфликтом имени полей DateStart и DateEnd

--- Nat.Web.Controls/BaseLogic/PeriodHistory.cs
+++ Nat.Web.Controls/BaseLogic/PeriodHistory.cs
@@ -4,21 +4,24 @@
  * Copyright © JSC New Age Technologies 2009
  */
 
-using System;
-using System.Collections.Generic;
-using System.Web.UI;
-using Nat.Web.Controls.GenerationClasses;
-
 namespace Nat.Web.Controls.BaseLogic
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Web.UI;
+
+    using Nat.Web.Controls.GenerationClasses;
+
     public class PeriodHistory : BaseLogic
     {
         public override void Logic()
         {
             if (IsStartAction && (IsEdit || IsNew))
             {
-                var property = ControlInfo.GetType().GetProperty("DateStart");
-                if (property == null) throw new Exception("Logic 'PeriodHistory' can not found property DateStart");
+                var property = ControlInfo.GetType().GetProperty("DateStart") ?? ControlInfo.GetType().GetProperty("dateStart");
+                if (property == null)
+                    throw new Exception("Logic 'PeriodHistory' can not found property DateStart");
+
                 property.SetValue(ControlInfo, DateTime.Now, null);
             }
         }
@@ -26,12 +29,14 @@ namespace Nat.Web.Controls.BaseLogic
         public override void InitClientManagmentControl(ICollection<BaseClientManagmentControl> managmentControls)
         {
             if (!IsNew) return;
-            var property = ControlInfo.GetType().GetProperty("DateEndControl");
-            if(property == null) throw new Exception("Logic 'PeriodHistory' can not found property DateEndControl");
+            var property = ControlInfo.GetType().GetProperty("DateEndControl") ?? ControlInfo.GetType().GetProperty("dateEndControl");
+            if (property == null)
+                throw new Exception("Logic 'PeriodHistory' can not found property DateEndControl");
+
             var item = new BaseClientManagmentControl();
             var control = (Control)property.GetValue(ControlInfo, null);
-            var enableItem = new EnableItem{TargetControl = control};
-            enableItem.EnableItems.Items.Add(new EnableByEmpty {Disable = true});
+            var enableItem = new EnableItem { TargetControl = control };
+            enableItem.EnableItems.Items.Add(new EnableByEmpty { Disable = true });
             item.EnableItems.Add(enableItem);
             managmentControls.Add(item);
         }

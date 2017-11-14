337ec272c37810293ec35a4f86a73f8aaf447fba
Sergey.Shpakovskiy <Sergey.Shpakovskiy@nat.kz>
2017-11-14 15:46:41 +0600

    Nat.Web.Tools - добавлен конверт с Rtf в Html

--- Nat.Web.Tools/Export/HtmlToRtf.cs
+++ Nat.Web.Tools/Export/HtmlToRtf.cs
@@ -83,4 +83,74 @@
             }
         }
     }
+    public static class RtfToHtml
+    {
+        private static readonly object LockObject = new object();
+
+        private static string _result;
+        private static Exception _exception;
+        
+        public static string ConvertRtfToHtml(string rtfText)
+        {
+            lock (LockObject)
+            {
+                _result = null;
+                _exception = null;
+                var thread = new Thread(ConvertRtfInSTAThread);
+                thread.SetApartmentState(ApartmentState.STA);
+                thread.Start(rtfText);
+                thread.Join(30000);
+                if (_exception != null)
+                    throw new TargetInvocationException(_exception);
+
+                if (string.IsNullOrEmpty(_result))
+                    return string.Empty;
+                
+                return _result;
+            }
+        }
+        
+        private static void ConvertRtfInSTAThread(object rtfText)
+        {
+            try
+            {
+                var richTextBox = new RichTextBox();
+                if (string.IsNullOrEmpty((string)rtfText))
+                {
+                    _result = "";
+                    return;
+                }
+                var textRange = new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd);
+                using (var rtfMemoryStream = new MemoryStream())
+                {
+                    using (var rtfStreamWriter = new StreamWriter(rtfMemoryStream))
+                    {
+                        rtfStreamWriter.Write(rtfText);
+                        rtfStreamWriter.Flush();
+                        rtfMemoryStream.Seek(0, SeekOrigin.Begin);
+                        textRange.Load(rtfMemoryStream, DataFormats.Rtf);
+                    }
+                }
+
+                string xaml;
+                using (var rtfMemoryStream = new MemoryStream())
+                {
+                    textRange = new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd);
+                    textRange.Save(rtfMemoryStream, DataFormats.Xaml);
+                    rtfMemoryStream.Seek(0, SeekOrigin.Begin);
+                    using (var rtfStreamReader = new StreamReader(rtfMemoryStream))
+                    {
+                        xaml = rtfStreamReader.ReadToEnd();
+                    }
+                }
+
+                _result = HtmlFromXamlConverter.ConvertXamlToHtml(xaml, false);
+            }
+            catch (Exception e)
+            {
+                _exception = e;
+                _result = null;
+            }
+        }
+    }
 }

8c195b60c370c626d7f8bd9b2548ff6ffb01e2fa
Sergey.Shpakovskiy <Sergey.Shpakovskiy@nat.kz>
2017-11-15 11:31:54 +0600

    Nat.Web.Tools - добавил класс для упрощения зависимостей кэша DictionaryCacheDependency и DictionaryRecordCacheDependency

---
+++ Nat.Web.Tools/DictionaryCacheDependency.cs
@@ -0,0 +1,100 @@
+﻿namespace Nat.Web.Tools
+{
+    using System;
+    using System.Web;
+    using System.Web.Caching;
+
+    /// <summary>
+    /// В зависимом классе TSource вызывается метод DictionaryCacheDependency&lt;TSource&gt;.Increcment(), при изменении, например, в partial DataSaved.
+    /// При добавлении кэша достаточно указать new DictionaryCacheDependency&lt;TSource&gt;().
+    /// </summary>
+    /// <typeparam name="TSource"></typeparam>
+    public class DictionaryCacheDependency<TSource> : CacheDependency
+    {
+        public DictionaryCacheDependency()
+            : base(null, new[] { GetKey() })
+        {
+            SetCache(0);
+        }
+
+        internal static void SetCache(int value)
+        {
+            var key = GetKey();
+
+            if (HttpContext.Current.Cache[key] == null)
+                HttpContext.Current.Cache.Add(
+                    key,
+                    value,
+                    null,
+                    DateTime.Today.AddDays(1),
+                    Cache.NoSlidingExpiration,
+                    CacheItemPriority.High,
+                    null);
+            else if (value != 0)
+                HttpContext.Current.Cache[key] = value;
+        }
+
+        internal static string GetKey()
+        {
+            return typeof(DictionaryCacheDependency<TSource>).Name + "[" + typeof(TSource).Name + "]";
+        }
+
+        public static int GetValue()
+        {
+            return (int?)HttpContext.Current.Cache[GetKey()] ?? 0;
+        }
+
+        public static void Increcment()
+        {
+            SetCache(GetValue() + 1);
+        }
+    }
+
+    public class DictionaryCacheDependency<TSource1, TSource2> : CacheDependency
+    {
+        public DictionaryCacheDependency()
+            : base(null, new[] { DictionaryCacheDependency<TSource1>.GetKey(), DictionaryCacheDependency<TSource2>.GetKey() })
+        {
+            DictionaryCacheDependency<TSource1>.SetCache(0);
+            DictionaryCacheDependency<TSource2>.SetCache(0);
+        }
+    }
+
+    public class DictionaryCacheDependency<TSource1, TSource2, TSource3> : CacheDependency
+    {
+        public DictionaryCacheDependency()
+            : base(
+                null,
+                new[]
+                    {
+                        DictionaryCacheDependency<TSource1>.GetKey(),
+                        DictionaryCacheDependency<TSource2>.GetKey(),
+                        DictionaryCacheDependency<TSource3>.GetKey()
+                    })
+        {
+            DictionaryCacheDependency<TSource1>.SetCache(0);
+            DictionaryCacheDependency<TSource2>.SetCache(0);
+            DictionaryCacheDependency<TSource3>.SetCache(0);
+        }
+    }
+
+    public class DictionaryCacheDependency<TSource1, TSource2, TSource3, TSource4> : CacheDependency
+    {
+        public DictionaryCacheDependency()
+            : base(
+                null,
+                new[]
+                    {
+                        DictionaryCacheDependency<TSource1>.GetKey(),
+                        DictionaryCacheDependency<TSource2>.GetKey(),
+                        DictionaryCacheDependency<TSource3>.GetKey(),
+                        DictionaryCacheDependency<TSource4>.GetKey()
+                    })
+        {
+            DictionaryCacheDependency<TSource1>.SetCache(0);
+            DictionaryCacheDependency<TSource2>.SetCache(0);
+            DictionaryCacheDependency<TSource3>.SetCache(0);
+            DictionaryCacheDependency<TSource4>.SetCache(0);
+        }
+    }
+}
---
+++ Nat.Web.Tools/DictionaryRecordCacheDependency.cs
@@ -0,0 +1,52 @@
+﻿namespace Nat.Web.Tools
+{
+    using System;
+    using System.Web;
+    using System.Web.Caching;
+
+    /// <summary>
+    /// В зависимом классе TSource вызывается метод DictionaryCacheDependency&lt;TSource&gt;.Increcment(id), при изменении, например, в partial DataSaved.
+    /// При добавлении кэша указать new DictionaryCacheDependency&lt;TSource&gt;(id).
+    /// </summary>
+    /// <typeparam name="TSource"></typeparam>
+    public class DictionaryRecordCacheDependency<TSource> : CacheDependency
+    {
+        public DictionaryRecordCacheDependency(string id)
+            : base(null, new[] { GetKey(id) })
+        {
+            SetCache(0, id);
+        }
+
+        internal static void SetCache(int value, string id)
+        {
+            var key = GetKey(id);
+
+            if (HttpContext.Current.Cache[key] == null)
+                HttpContext.Current.Cache.Add(
+                    key,
+                    value,
+                    null,
+                    DateTime.Today.AddDays(1),
+                    Cache.NoSlidingExpiration,
+                    CacheItemPriority.High,
+                    null);
+            else if (value != 0)
+                HttpContext.Current.Cache[key] = value;
+        }
+
+        internal static string GetKey(string id)
+        {
+            return typeof(DictionaryRecordCacheDependency<TSource>).Name + "[" + typeof(TSource).Name + "]:" + id;
+        }
+
+        public static int GetValue(string id)
+        {
+            return (int?)HttpContext.Current.Cache[GetKey(id)] ?? 0;
+        }
+
+        public static void Increcment(string id)
+        {
+            SetCache(GetValue(id) + 1, id);
+        }
+    }
+}

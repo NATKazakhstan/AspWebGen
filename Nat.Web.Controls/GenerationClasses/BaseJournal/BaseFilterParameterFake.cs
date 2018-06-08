c036d416635290c5115b1ab95f10f2aef5f4a0f9
Sergey.Shpakovskiy <Sergey.Shpakovskiy@nat.kz>
2018-06-08 17:03:13 +0600

    Nat.Web.Controls - добавлен фильтр с отключенной фильтрацией

---
+++ Nat.Web.Controls/GenerationClasses/BaseJournal/BaseFilterParameterFake.cs
@@ -0,0 +1,23 @@
+﻿namespace Nat.Web.Controls.GenerationClasses.BaseJournal
+{
+    using System;
+    using System.Linq;
+    using System.Linq.Expressions;
+
+    using Nat.Web.Controls.GenerationClasses.Filter;
+
+    public class BaseFilterParameterFake<TTable, TField> : BaseFilterParameter<TTable, TField>
+        where TTable : class
+        where TField : struct
+    {
+        protected internal override Expression OnFilter(Enum filtertype, FilterItem filterItem, QueryParameters qParams)
+        {
+            return null;
+        }
+
+        protected override IQueryable OnFilter(IQueryable query, Enum filtertype, string value1, string value2)
+        {
+            return query;
+        }
+    }
+}

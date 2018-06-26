e71905c0748462d6bf6bea037d79d65b71b58241
Sergey.Shpakovskiy <Sergey.Shpakovskiy@nat.kz>
2018-06-26 12:28:11 +0600

    Nat.Web.Tools.ExtNet добавлен класс DecimalConverter для обработки сохранения чисел из строки стора

---
+++ Nat.Web.Tools.ExtNet/Json/DecimalConverter.cs
@@ -0,0 +1,43 @@
+﻿namespace Nat.Web.Tools.ExtNet.Json
+{
+    using System;
+
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+
+    public class DecimalConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(decimal) || objectType == typeof(decimal?);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var token = JToken.Load(reader);
+            if (token.Type == JTokenType.Float || token.Type == JTokenType.Integer)
+                return token.ToObject<decimal>();
+
+            if (token.Type == JTokenType.String)
+            {
+                var str = token.ToString(); /*
+                if (CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator == ".")
+                    str = str.Replace(",", ".");
+                else if (CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator == ",")
+                    str = str.Replace(".", ",");*/
+
+                return Convert.ToDecimal(str);
+            }
+
+            if (token.Type == JTokenType.Null && objectType == typeof(decimal?))
+                return null;
+
+            throw new JsonSerializationException("Unexpected token type: " + token.Type);
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

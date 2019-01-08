6e6aac4fc23b06ff6431b0d6e482e384e80d852e
Sergey.Shpakovskiy <Sergey.Shpakovskiy@nat.kz>
2019-01-08 13:15:40 +0600

    Nat.Web.Tools.ExtNet добавлены методы для сортировки

--- Nat.Web.Tools.ExtNet/Extenders/BaseDataSourceViewExtender.cs
+++ Nat.Web.Tools.ExtNet/Extenders/BaseDataSourceViewExtender.cs
@@ -14,6 +14,7 @@ namespace Nat.Web.Tools.ExtNet.Extenders
 
     using Ext.Net;
 
+    using Nat.Web.Controls;
     using Nat.Web.Controls.GenerationClasses;
 
     public static class BaseDataSourceViewExtender
@@ -63,6 +64,58 @@ namespace Nat.Web.Tools.ExtNet.Extenders
             var sortExpression =
                 sort.Select(r => dictionary[r.Property].Sort + (r.Direction == SortDirection.DESC ? " desc" : string.Empty));
             return string.Join(",", sortExpression);
+        }
+
+        /// <summary>
+        /// Сортировка данных на стороне SQL сервера.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="data"></param>
+        /// <param name="columns"></param>
+        /// <param name="sort"></param>
+        /// <returns></returns>
+        public static IQueryable<T> SortBy<T>(this IQueryable<T> data, BaseGridColumns columns, ICollection<DataSorter> sort)
+            where T : class
+        {
+            var dictionary = columns.Columns.OfType<GridColumn>().ToDictionary(r => r.ColumnNameIndex);
+            var sortExpression = string.Join(
+                ",",
+                sort
+                    .Where(r => dictionary.ContainsKey(r.Property) && !string.IsNullOrEmpty(dictionary[r.Property].Sort))
+                    .Select(r => dictionary[r.Property].Sort + (r.Direction == SortDirection.DESC ? " desc" : string.Empty)));
+
+            if (string.IsNullOrEmpty(sortExpression))
+                return data;
+
+            return LinqFilterGenerator.OrderBy(data, sortExpression);
+        }
+
+        /// <summary>
+        /// Сортировка данных после выполнения SQL запроса.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="data"></param>
+        /// <param name="columns"></param>
+        /// <param name="sort"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> SortBy<T>(this IEnumerable<T> data, BaseGridColumns columns, ICollection<DataSorter> sort)
+            where T : class
+        {
+            var dictionary = columns.Columns.OfType<GridColumn>().ToDictionary(r => r.ColumnNameIndex);
+            var sortExpression = string.Join(
+                ",",
+                sort
+                    .Where(r => dictionary.ContainsKey(r.Property))
+                    .Select(
+                        r => (string.IsNullOrEmpty(dictionary[r.Property].Sort)
+                                  ? dictionary[r.Property].ServerMapping
+                                  : "Item." + dictionary[r.Property].Sort)
+                             + (r.Direction == SortDirection.DESC ? " desc" : string.Empty)));
+
+            if (string.IsNullOrEmpty(sortExpression))
+                return data;
+
+            return LinqFilterGenerator.OrderBy(data.AsQueryable(), sortExpression);
         }
     }
 }

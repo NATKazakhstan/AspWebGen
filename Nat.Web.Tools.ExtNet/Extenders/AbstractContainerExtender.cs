806f041f0a0c8ed9609ee6ed695845a923f05063
Sergey.Shpakovskiy <Sergey.Shpakovskiy@nat.kz>
2019-11-28 19:35:18 +0600

    Nat.Web.Tools.ExtNet исправил проблему скрытия гридов на стороне сервера

--- Nat.Web.Tools.ExtNet/Extenders/AbstractContainerExtender.cs
+++ Nat.Web.Tools.ExtNet/Extenders/AbstractContainerExtender.cs
@@ -8,17 +8,23 @@ namespace Nat.Web.Tools.ExtNet.Extenders
         public static void SetHiddenControlContainer(AbstractComponent control)
         {
             var abstractContainer = control.Parent as AbstractContainer ?? control.ParentComponent as AbstractContainer;
-            abstractContainer.SetHiddenControlContainer();
+            abstractContainer.SetHiddenControlContainer(control.Hidden);
         }
 
-        private static void SetHiddenControlContainer(this AbstractContainer container)
+        private static void SetHiddenControlContainer(this AbstractContainer container, bool newHidden)
         {
             if (container == null) return;
 
-            var hidden = container.Items.Where(c => c.GetType() != typeof(Hidden)).All(c => c.Hidden);
-            container.Hidden = hidden;
-            var abstractContainer = container.Parent as AbstractContainer ?? container.ParentComponent as AbstractContainer;
-            abstractContainer.SetHiddenControlContainer();
+            var components = container.Items.Where(c => c.GetType() != typeof(Hidden)).ToArray();
+            if (components.Length == 0)
+                container.Hidden = newHidden;
+            else if (components.All(c => c.Hidden) && newHidden)
+                container.Hidden = true;
+            else
+                container.Hidden = false;
+
+            var abstractContainer = container.Parent as AbstractContainer;
+            abstractContainer.SetHiddenControlContainer(container.Hidden);
         }
     }
 }

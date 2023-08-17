e4bb73e35879e74d79b63c933f2816fda948b2db
Vladimir Kovalev <Vladimir.Kovalev@nat.kz>
2023-08-17 12:50:17 +0600

    Поправлен интерфейс IReportQr

--- Nat.Web.Tools/Report/IReportQr.cs
+++ Nat.Web.Tools/Report/IReportQr.cs
@@ -4,6 +4,7 @@ namespace Nat.Web.Tools.Report
 {
     public interface IReportQr
     {
+        void AddToExcel(Stream stream, long logId);
         void AddToExcel(Stream stream, long logId, string pluginFullName);
     }
 }
